4dd53460979900a27fe285646ba9615312fd447a
LukaDivjakovic <lukadivus@gmail.com>
2025-11-01 23:01:10 +0100

    commit nakon prvog Junie builda

---
+++ src/main/kotlin/lukaluka/searchplugin/forms/FileSearch.java
@@ -0,0 +1,15 @@
+package lukaluka.searchplugin.forms;
+
+import javax.swing.*;
+
+public class FileSearch {
+    private JPanel contentPane;
+    private JTextField directoryPathField;
+    private JTextField stringToSearchField;
+    private JButton startSearchButton;
+    private JButton cancelSearchButton;
+
+    public JPanel getContentPane() {
+        return contentPane;
+    }
+}
---
+++ src/main/kotlin/lukaluka/searchplugin/toolwindow/SearchToolWindowFactory.kt
@@ -0,0 +1,17 @@
+package lukaluka.searchplugin.toolwindow
+
+import com.intellij.openapi.project.DumbAware
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.wm.ToolWindow
+import com.intellij.openapi.wm.ToolWindowFactory
+import com.intellij.ui.content.ContentFactory
+import lukaluka.searchplugin.forms.FileSearch
+
+class SearchToolWindowFactory : ToolWindowFactory, DumbAware {
+    override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
+        val form = FileSearch()
+        val panel = form.contentPane
+        val content = ContentFactory.getInstance().createContent(panel, null, false)
+        toolWindow.contentManager.addContent(content)
+    }
+}
